23e9cd1e82819687976ed0ca3be53a991e11413d
Pratik Sethia <pratiksethia1@gmail.com>
2016-04-03 16:25:32 +0530

    3 cleanup

--- resources/views/maincontent.blade.php
+++ resources/views/maincontent.blade.php
@@ -1,6 +1,6 @@
 @extends('pages.dashboard')
 
-@section('contentfourbox')
+@section('contentFourBox')
 		<div class="box box-default">
 			<div class="box-header with-border">
 				<h3 class="box-title">Overall Statistics</h3>
@@ -134,8 +134,195 @@
 				</table>
 			</div><!-- /.box-body -->
 		</div><!-- /.box -->
-                </div><!-- /.col -->
-              </div><!-- /.row -->
-            </section><!-- /.content -->
-          </div><!-- /.content-wrapper -->
+@endsection
+@section('contentLiveGraphHead')
+	<script type="text/javascript">
+		$(function () {
+			var datePreviouslyTaken,dateCheck=0,dateCurrentOne;
+			var feedback = <?php echo json_encode($dataForPreviousValues); ?>;
+			{{----}}
+//                    document.writeln(feedback);
+			var feedbackLength = feedback.length;
+			datePreviouslyTaken = Date.createFromMysql(feedback[feedbackLength - 1].dateTime);
+			var parameterName = "kWH";
+			//Following will check if URL parameter exits or not.
+			var field = 'pname';
+			var url = window.location.href;
+			if(url.indexOf('?' + field + "=power") != -1 || url.indexOf('&' + field + "=power") != -1) {
+				parameterName = "kWH";
+			}
+			else if(url.indexOf('?' + field + "=electricity") != -1 || url.indexOf('&' + field + "=electricity") != -1) {
+				parameterName = "ampere";
+			}
+			else if(url.indexOf('?' + field + "=production") != -1 || url.indexOf('&' + field + "=production") != -1) {
+				parameterName = "degreeC";
+			}
+			else if(url.indexOf('?' + field + "=water") != -1 || url.indexOf('&' + field + "=water") != -1) {
+				parameterName = "bar";
+			}
+			else
+				parameterName = "kWH";
+			//document.writeln(feedback[1].kWH);
+			Highcharts.setOptions({
+				global : {
+					useUTC : false
+				}
+			});
+			// Create the chart
+			$('#chartContainer').highcharts('StockChart', {
+				chart : {
+					events : {
+						load : function () {
+							// set up the updating of the chart each second
+							var series = this.series[0];
+							setInterval(function () {
+								var feedbackLive ;
+								feedbackLive = $.ajax({
+									type: "POST",
+									cache: false,
+									url: "/liveGraphValues",
+									async: false
+								}).success(function(){
+									setTimeout(function(){get_fb_success();}, 500);
+								}).responseText;
+								//document.write("<br>feedback is " + feedbackLive  );
+								function get_fb_success() {
+									if(feedbackLive != "[]")
+									{
+										feedbackLive = JSON.parse(feedbackLive);
+										var graphJSONincrementer = 0;
+										//Folowing is to initialize HTML elements with particular ids, so as to prevent error!
+										window.onload = settingElements();
+										function settingElements() {
+											document.getElementById("kwh").innerHTML = parseInt(feedbackLive[graphJSONincrementer].kWH);
+											document.getElementById("ampere").innerHTML = parseInt(feedbackLive[graphJSONincrementer].ampere);
+											document.getElementById("degreeC").innerHTML = parseInt(feedbackLive[graphJSONincrementer].degreeC);
+											document.getElementById("bar").innerHTML = parseInt(feedbackLive[graphJSONincrementer].bar);
+											document.getElementById("kwhMoney").innerHTML = (parseInt(feedbackLive[graphJSONincrementer].kWH)*6.986).toFixed(2);
+										}
+										//Following will check, if value is missed or not with the help of date... If missed then the page will be reloaded.
+										dateCurrentOne = Date.createFromMysql(feedbackLive[graphJSONincrementer].dateTime);
+										var subDate = dateCurrentOne - datePreviouslyTaken;
+										if(subDate <15500)  {
+											if(parameterName == "kWH") {
+												var x = Number(Date.createFromMysql(feedbackLive[graphJSONincrementer].dateTime)), // current time
+														y = feedbackLive[graphJSONincrementer].kWH;
+											} else if(parameterName == "ampere") {
+												var x = Number(Date.createFromMysql(feedbackLive[graphJSONincrementer].dateTime)), // current time
+														y = feedbackLive[graphJSONincrementer].ampere;
+											} else if(parameterName == "degreeC") {
+												var x = Number(Date.createFromMysql(feedbackLive[graphJSONincrementer].dateTime)), // current time
+														y = feedbackLive[graphJSONincrementer].degreeC;
+											} else if(parameterName == "bar") {
+												var x = Number(Date.createFromMysql(feedbackLive[graphJSONincrementer].dateTime)), // current time
+														y = feedbackLive[graphJSONincrementer].bar;
+											}
+											series.addPoint([x, y], true, true);
+											graphJSONincrementer++;
+											datePreviouslyTaken = dateCurrentOne;
+										}
+										else
+											location.reload();
+									}
+								}}, 4000);
+						}
+					}
+				},
+
+				rangeSelector: {
+					buttons: [{
+						count: 1,
+						type: 'minute',
+						text: '1M'
+					}, {
+						count: 5,
+						type: 'minute',
+						text: '5M'
+					}, {
+						count: 30,
+						type: 'minute',
+						text: '30M'
+					}, {
+						count: 1,
+						type: 'hour',
+						text: '1H'
+					}, {
+						count: 3,
+						type: 'hour',
+						text: '3H'
+					},{
+						count: 6,
+						type: 'hour',
+						text: '6H'
+					}, {
+						type: 'sll',
+						text: 'ALL'
+					}],
+					inputEnabled: false,
+					selected: 1
+				},
+
+				title : {
+					text : ''
+				},
+
+				exporting: {
+					enabled: false
+				},
+
+				series : [{
+					name : parameterName,
+					data : (function () {
+						// generate graph for previousData
+						window.onload = settingElements(); //Sets Tile values to the latest recent value
+						function settingElements() {
+							document.getElementById("kwh").innerHTML = parseInt(feedback[feedbackLength - 1].kWH);
+							document.getElementById("ampere").innerHTML = parseInt(feedback[feedbackLength - 1].ampere);
+							document.getElementById("degreeC").innerHTML = parseInt(feedback[feedbackLength - 1].degreeC);
+							document.getElementById("bar").innerHTML = parseInt(feedback[feedbackLength - 1].bar);
+							document.getElementById("kwhMoney").innerHTML = (parseInt(feedback[feedbackLength - 1].kWH)*6.986).toFixed(2);
+						}
+
+						var data = [], i;
+						//Pushes values/data to graph to display
+						for (i = -(feedbackLength-1),j=0; i <= 0 ; i += 1,j++) {
+							if(parameterName == "kWH") {
+								data.push([
+									Number(Date.createFromMysql(feedback[j].dateTime)),
+									parseInt(feedback[j].kWH)
+								]);
+							} else if(parameterName == "ampere") {
+								data.push([
+									Number(Date.createFromMysql(feedback[j].dateTime)),
+									parseInt(feedback[j].ampere)
+								]);
+							} else if(parameterName == "degreeC") {
+								data.push([
+									Number(Date.createFromMysql(feedback[j].dateTime)),
+									parseInt(feedback[j].degreeC)
+								]);
+							} else if(parameterName == "bar") {
+								data.push([
+									Number(Date.createFromMysql(feedback[j].dateTime)),
+									parseInt(feedback[j].bar)
+								]);
+							}
+						}
+						return data;
+					}())
+				}]
+			});
+
+		});
+
+		Date.createFromMysql = function(mysql_string)  //Converts SQL DateTime format to Javascript format.
+		{
+			if(typeof mysql_string === 'string')
+			{
+				var t = mysql_string.split(/[- :]/);
+				return new Date(t[0], t[1] - 1, t[2], t[3] || 0, t[4] || 0, t[5] || 0);
+			}
+			return null;
+		}
+	</script>
 @endsection
