307ee1a36824c72e9c6af23f5f8d6824136222da
nikhilbhatia22 <geekynikhil@gmail.com>
2016-04-10 00:01:15 +0530

    Column & Parameter Mapping!
    NOTE 1: Project might not work, as hierarchy in the dashboard is pointed
    to company named "N2P2 Pvt. Ltd." So, first add this company into
    database with csv file named "N2P2 Pvt. Ltd." [I have mailed this file]
    and give company name : "N2P2 Pvt. Ltd."
    
    NOTE 2: I am also mailing you csvDataParser coded based on this
    mapping.... to execute that parser, you are first required to map meters
    of company "N2P2 Pvt. Ltd."
    
    Description for the changes is as under:
    
    =====File : MappingController.php=====
    New Controller is created for mapping function... Currently, it has 3
    functions:
    1. mappingIndexPage() : This function fetches list of companies present
    in DB and this list is passed to index page of mapping
    ('mappingIndex.blade.php' is index page of mapping), where these
    companies are shown as a drop down list....
    
    2. mappingSelectionPage() : This function is invoked when company is
    selected on index page of mapping... When user selects company then this
    controller fetches some info from database, makes an array for the data
    fetched from database and passes this array to 'mappingSelectionPage'
    view.
    Data that are fetched from DB in this function are
    a. meters (leaf node in db) name and their id
    b. parameter names and ids present in table 'parameters_details'
    
    3. mappingSubmit() : This is the last function for mapping, it inserts
    necessary data given by user in to database... It is invoked after user
    submits form of page 'mappingSelectionPage'
    
    =====File : routes.php=====
    necessary routes for mapping are created....
    
    =====File : parameterDetails.php=====
    new eloquent for table 'parameter_details'
    
    =====File : mappingIndex.blade.php=====
    Its an index page for mapping showing drop down list of companies in DB
    
    =====File : mappingSelectionPage.blade.php=====
    This is 2nd page for mapping... it shows all meters (leaf nodes) against
    one textbox (to map csv data columns) and one drop-down menu (to map
    parameters for the meter)
    
    Thats it!!!!

---
+++ app/parameterDetails.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: NIKHIL
+ * Date: 09-04-2016
+ * Time: 09:58 PM
+ */
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model as Eloquent;
+
+class parameterDetails extends Eloquent{
+    protected $table ='parameter_details';
+} 
